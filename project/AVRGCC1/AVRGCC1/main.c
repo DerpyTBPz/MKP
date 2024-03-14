8411cf7e36e98f7ed52ec461d7da0b010a110056
DerpyTBPz <dima.plyasov13@gmail.com>
2024-03-14 13:30:07 +0200

    lab1_update

--- project/AVRGCC1/AVRGCC1/main.c
+++ project/AVRGCC1/AVRGCC1/main.c
@@ -8,55 +8,79 @@
 #include <avr/delay.h>
 #include <avr/interrupt.h>
 
+/* board
+#define		RED		0x80
+#define		GREEN	0x20
+#define		BLUE	0x10 
+#define		BUT1	0x04
+#define		BUT2	0x0
+*/
+
 #define		RED		0x80
 #define		GREEN	0x20
 #define		BLUE	0x10 
 #define		BUT1	0x04
 #define		BUT2	0x08
 
+// port change for simulator (comment on board)
+// #define		DDRD	DDRA
+// #define		PORTD	PORTA
+
+
+
 void task1();
 void task2();
 void task3();
 void task4();
+void task5();
+void task6();
 
+void test();
+
+// BUT1 INT
 ISR(INT0_vect)
 {
 	PORTD ^= BLUE;	
 }
 
+// BUT2 INT
+ISR(INT1_vect)
+{
+	PORTD ^= RED;
+}
+
+ISR_ALIASOF(INT1_vect, INT0_vect)
+{
+	PORTD ^= RED;
+};
+
+
 int main(void)
 {
-	//int flag = 0;
-	//DDRD |= RED | GREEN | BLUE | BUT1;
-	//task4();
-	//.PORTD = BLUE;
-	//_delay_ms(1000);
-	
 // 	task1();
 // 	task2();
 // 	task3();
 // 	task4();
- 	task5();
+// 	task5();
+	task6();
+	 
+//	test();
 		
     while(1)
-    {		
-		
-		//task1();
-		//task2();
-		//task3(flag);
-		
-				
-		
+    {	
         //TODO:: Please write your application code 
 	}
 }
 
 void task1()
 {
-	DDRD |= RED;
+	DDRD |= GREEN;
 	while(1)
 	{
-		PORTD = RED;
+		_delay_ms(1000);
+		PORTD = GREEN;
+		_delay_ms(1000);
+		PORTD = 0x00;
 	}
 }
 
@@ -122,14 +146,47 @@ void task3()
 void task4()
 {
 	DDRD = 0xB0;
-	MCUCR = 0x03;
-	GICR = 0x40;
+	MCUCR = 0x03; //0x0F 0x03
+	GICR = 0x40; //0xC0 0x40
 	sei();	
 }
 
 void task5()
 {
 	DDRD |= RED | GREEN | BLUE | BUT1 | BUT2;
+	while(1)
+	{
+		if (PIND & BUT1)
+		{
+			PORTD = BLUE;
+		}
+		else if (PIND & BUT2)
+		{
+			PORTD = GREEN;		
+		}
+		else if (PIND & (BUT1 | BUT2))
+		{
+			PORTD = RED;
+		}
+		else
+		{
+			PORTD = 0x0;
+		}
+	}	
+}
+
+void task6()
+{
+	DDRD = 0xB0;
+	MCUCR = 0x0F; //0x0F 0x03
+	GICR = 0xC0; //0xC0 0x40
+	sei();	
+}
+
+void test()
+{
+	DDRD |= RED | GREEN | BLUE | BUT1 | BUT2;
+	PIND |= BUT1 | BUT2;
 	while(1)
 	{
 		if (PIND & BUT1)
