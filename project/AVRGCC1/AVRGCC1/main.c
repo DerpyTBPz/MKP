b0d38aa0ffed3ae1c824e021766bd687c4bcae59
DerpyTBPz <dima.plyasov13@gmail.com>
2024-03-28 18:46:13 +0200

    lab1

--- project/AVRGCC1/AVRGCC1/main.c
+++ project/AVRGCC1/AVRGCC1/main.c
@@ -16,13 +16,13 @@
 #define		BUT2	0x08
 */
 
-#define		RED		0x80
+#define		RED		0x10
 #define		GREEN	0x20
-#define		BLUE	0x10 
+#define		BLUE	0x80 
 #define		BUT1	0x04
 #define		BUT2	0x08
 
-// port change for simulator (comment on board)
+// port change for simulator (remove on board)
 // #define		DDRD	DDRA
 // #define		PORTD	PORTA
 
@@ -60,7 +60,7 @@ ISR(INT0_vect)
 	if ((PIND & BUT1) && (PIND & BUT2))
 	{
 //		PORTD ^= 0x20;
-		PORTD ^= GREEN;
+		PORTD ^= RED;
 	}
 	else 
 	{
@@ -78,7 +78,7 @@ ISR(INT1_vect)
 	}
 	else 
 	{
-		PORTD ^= RED; 		
+		PORTD ^= GREEN; 		
 	}
 //	PORTD ^= RED;
 }
@@ -138,8 +138,7 @@ void task2()
 
 void task3()
 {
-	DDRD |= RED | GREEN | BLUE | BUT1;
-	/*DDRD &= ~BUT1;*/
+	DDRD |= RED | GREEN | BLUE | BUT1;	
 
 	while(1)
 	{
@@ -193,26 +192,54 @@ void task5()
 {
 	DDRD |= RED | GREEN | BLUE | BUT1 | BUT2;	
 	while(1)
-	{
-		if ((PIND & BUT1) && (PIND ^ BUT2))
-		{
-			_delay_ms(70);
-			PORTD ^= BLUE;
-		}
-		else if ((PIND & BUT2) && (PIND ^ BUT1))
-		{
-			_delay_ms(70);
-			PORTD ^= GREEN;
-		}
-		else if ((PIND & BUT1) && (PIND & BUT2))
-		{
-			_delay_ms(70);
-			PORTD ^= RED;
-		}		
+	{		
+		if ((PIND & BUT1) || (PIND & BUT2) || ((PIND & BUT1) && (PIND & BUT2)))
+ 		{
+			if ((PIND & BUT1) && (PIND & BUT2)) 
+			{
+				PORTD ^= RED;
+				_delay_ms(300);			
+			}
+			else if ((PIND & BUT2))
+			{
+				PORTD ^= GREEN;
+				_delay_ms(300);			
+			}
+			else if ((PIND & BUT1))
+			{
+				PORTD ^= BLUE;
+				_delay_ms(300);		
+			}			
+ 		}
+		
+		
+// 		if ((PIND & BUT1) || (PIND & BUT2) || ((PIND & BUT1) && (PIND & BUT2)))
+// 		{
+// 			_delay_ms(100);			
+// 		}
+		
+// 		if ((PIND & BUT1) && (PIND ^ BUT2)) 
+// 		{
+// 			//_delay_ms(70);
+// 			PORTD ^= BLUE;
+// 		}
+// 		else if ((PIND & BUT2) && (PIND ^ BUT1))
+// 		{
+// 			//_delay_ms(70);
+// 			PORTD ^= GREEN;
+// 		}
+// 		else if ((PIND & BUT1) && (PIND & BUT2))
+// 		{
+// 			//_delay_ms(70);
+// 			PORTD ^= RED;
+// 		}
+		//---tst
+		
+				
 	}	
 }
 
-void task7()
+void task6()
 {
 	DDRD = 0xB0;
 	DDRD |= RED | GREEN | BLUE | BUT1 | BUT2;
@@ -220,6 +247,9 @@ void task7()
 	GICR = 0xC0; //0xC0 0x40
 	sei();	
 }
+
+
+
 
 void test()
 {
