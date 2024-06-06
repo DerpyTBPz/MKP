5b9108190ed6a5c489ec3e66ba287dff40bf4796
derpytbpz <dima.plyasov13@gmai.com>
2024-06-06 11:30:31 +0300

    lab6

---
+++ project/AVRGCC7/AVRGCC7/main.c
@@ -0,0 +1,16 @@
+/*
+ * AVRGCC7.c
+ *
+ * Created: 06.06.2024 8:58:40
+ *  Author: Administrator
+ */ 
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
